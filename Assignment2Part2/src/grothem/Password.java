cdb86f766a620719cedd51dffebe66910c747bff
Joshua Grothem <joshua.grothem@gmail.com>
2022-09-18 21:22:01 -0700

    Indented and formatted

--- Assignment2Part2/src/grothem/Password.java
+++ Assignment2Part2/src/grothem/Password.java
@@ -14,30 +14,26 @@ public class Password {
 	}
 
 	public String blockSize() {
-		//input validation
-		if(password.length() <8 || password.length() >12) {
+		// input validation
+		if (password.length() < 8 || password.length() > 12) {
 			return "Invalid Input. Must be larger than 8 and smaller than 12";
 		}
-		
-		int count = 1; 
-	    int temp = 1;
-	    
-	    for (int i = 1; i < password.length(); i++)
-	    {
-	        //increase temp if same char (case sensitive)
-	        if (password.charAt(i) == password.charAt(i - 1))
-	        {
-	            temp++;
-	        }
-	        else
-	        {
-	        	//count is our answer and we make it the bigger num between
-	        	//temp and count
-	            count = Math.max(count, temp);
-	            temp = 1;
-	        }
-	    }
-	    count = Math.max(count, temp);
+
+		int count = 1;
+		int temp = 1;
+
+		for (int i = 1; i < password.length(); i++) {
+			// increase temp if same char (case sensitive)
+			if (password.charAt(i) == password.charAt(i - 1)) {
+				temp++;
+			} else {
+				// count is our answer and we make it the bigger num between
+				// temp and count
+				count = Math.max(count, temp);
+				temp = 1;
+			}
+		}
+		count = Math.max(count, temp);
 
 		if (count <= 2) {
 			suggestion = decent;
